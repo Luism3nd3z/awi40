f2cb2e312b99ded9bd520a0b6269deefd254e46c
Luism3nd3z <1720110403@utectulancingo.edu.mx>
2022-04-08 14:41:55 +0000

    a

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import web # libreria para usar el framework web.py
 import pyrebase # libreria para contecarse con firebase
 import firebase_config as token # archivo con la configuracion de firebase
 import json # libreria para manejar el formato JSON
-import pymongo
 
 urls = (
     '/', 'Login',
